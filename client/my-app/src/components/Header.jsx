16b5fd817770d0671f0b45650614d39686d79ffa
Suhasr76 <56556749+Suhasr76@users.noreply.github.com>
2021-11-19 19:43:29 +0530

    Updated `client` part of the web application

---
+++ client/my-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="font-weight-bold  display-1 text-center">Warehouse Details</h1>
+        </div>
+    )
+}
+
+export default Header
